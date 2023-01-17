b80f5b5e528b48f6d66bcbdda73f3292de695197
Susan Vanderplas <srvanderplas@gmail.com>
2023-01-17 14:51:25 -0600

    data and documentation

---
+++ code/WHO-incidence.R
@@ -0,0 +1,7 @@
+# WHO Disease incidence data
+
+# Tidy version: https://whowiise.blob.core.windows.net/upload/incidence-rate--2021.xlsx
+
+# Wider site: https://immunizationdata.who.int/listing.html?topic=&location=
+
+# Raw data saved is from 2020, before site migration and data format changes.
