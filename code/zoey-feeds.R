6f0e512dc9feeeaf39438ea6ee2f649302fcca44
srvanderplas <srvanderplas@gmail.com>
2023-01-23 11:54:59 -0600

    Include some fun data

--- code/zoey-feeds.R
+++ code/zoey-feeds.R
@@ -11,7 +11,7 @@ zoey_feeds <- read_csv("raw/zoey-feeds.csv") %>%
          end = as.POSIXct(`End Time`, tz = "America/Chicago", format = "%H:%M:%S %m-%d-%Y"))
 
 # Select first month of life
-zoey_first_month <- filter(zoey_feeds, start < as.POSIXct("2021-08-07") + days(30)) %>%
+zoey_initial <- filter(zoey_feeds, start < as.POSIXct("2021-08-07") + days(90)) %>%
   select(id, Start = `Start Time`, End = `End Time`, Type = `Feed Type`, matches("Quantity")) %>%
   mutate(Type = str_replace_all(Type, c("(Left|Right) " = "")))
-write_csv(zoey_first_month, "raw/feeds_first_month.csv")
+write_csv(zoey_initial, "raw/feeds_initial.csv")
