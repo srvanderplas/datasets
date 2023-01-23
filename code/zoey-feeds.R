46ecf84998a16f2c970c50b9e98e4ca8ddf80c35
srvanderplas <srvanderplas@gmail.com>
2023-01-23 11:53:32 -0600

    Fix data a bit

--- code/zoey-feeds.R
+++ code/zoey-feeds.R
@@ -4,6 +4,7 @@
 library(readr)
 library(dplyr)
 library(lubridate)
+library(stringr)
 
 zoey_feeds <- read_csv("raw/zoey-feeds.csv") %>%
   mutate(start = as.POSIXct(`Start Time`, tz = "America/Chicago", format = "%H:%M:%S %m-%d-%Y"),
@@ -11,5 +12,6 @@ zoey_feeds <- read_csv("raw/zoey-feeds.csv") %>%
 
 # Select first month of life
 zoey_first_month <- filter(zoey_feeds, start < as.POSIXct("2021-08-07") + days(30)) %>%
-  select(id, Start = `Start Time`, End = `End Time`, Food = `Food Type`, matches("Quantity"))
+  select(id, Start = `Start Time`, End = `End Time`, Type = `Feed Type`, matches("Quantity")) %>%
+  mutate(Type = str_replace_all(Type, c("(Left|Right) " = "")))
 write_csv(zoey_first_month, "raw/feeds_first_month.csv")
