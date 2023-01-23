f10b9bdb4acafe802059cc8a85bcf2b2dc3df469
srvanderplas <srvanderplas@gmail.com>
2023-01-23 10:56:41 -0600

    Feeds for Zoey - date time data

---
+++ code/zoey-feeds.R
@@ -0,0 +1,15 @@
+# Feed the Baby data (Zoey)
+# Exported from Feed Baby Android App on 2023-01-23
+
+library(readr)
+library(dplyr)
+library(lubridate)
+
+zoey_feeds <- read_csv("raw/zoey-feeds.csv") %>%
+  mutate(start = as.POSIXct(`Start Time`, tz = "America/Chicago", format = "%H:%M:%S %m-%d-%Y"),
+         end = as.POSIXct(`End Time`, tz = "America/Chicago", format = "%H:%M:%S %m-%d-%Y"))
+
+# Select first month of life
+zoey_first_month <- filter(zoey_feeds, start < as.POSIXct("2021-08-07") + days(30)) %>%
+  select(id, Start = `Start Time`, End = `End Time`, Food = `Food Type`, matches("Quantity"))
+write_csv(zoey_first_month, "raw/feeds_first_month.csv")
