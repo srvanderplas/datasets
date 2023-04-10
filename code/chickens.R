06ce3699bbee01605b12cb582c6e670b05afa52d
srvanderplas <srvanderplas@gmail.com>
2023-04-10 16:01:20 -0500

    Updates with SingleFile extension

--- code/chickens.R
+++ code/chickens.R
@@ -9,7 +9,7 @@ breeds$link <- read_html(url) %>%
   html_attr("href") %>%
   paste0("https://www.cacklehatchery.com", .)
 
-download.file(url, "raw/chickens/breeds-table.html")
+# download.file(url, "raw/chickens/breeds-table.html")
 
 library(dplyr)
 library(stringr)
@@ -17,8 +17,9 @@ breeds <- breeds %>%
   mutate(name = str_replace_all(`Chicken Breed Name`, c("[[:punct:]]" = "", " " = "-", "[^A-z-]"=""))) %>%
   mutate(outfile = paste0("raw/chickens/", name, ".html"))
 
+# 10 files saved with the SingleFile chrome extension, in order to capture the entire HTML after JavaScript injection.
 
-purrr::walk2(breeds$link, breeds$outfile, ~read_html(.x) %>% xml2::write_html(.y))
-html <- read_html(url)
+# purrr::walk2(breeds$link, breeds$outfile, ~read_html(.x) %>% xml2::write_html(.y))
+# html <- read_html(url)
 
-download.file(breeds$link, destfile = paste0("raw/chickens/", breeds$name, ".html"))
+# download.file(breeds$link, destfile = paste0("raw/chickens/", breeds$name, ".html"))
