a1c26b850d7857dea1963c67b61c0f65dcb6369f
srvanderplas <srvanderplas@gmail.com>
2023-04-10 15:33:54 -0500

    Updates

--- code/chickens.R
+++ code/chickens.R
@@ -14,6 +14,11 @@ download.file(url, "raw/chickens/breeds-table.html")
 library(dplyr)
 library(stringr)
 breeds <- breeds %>%
-  mutate(name = str_replace_all(`Chicken Breed Name`, c("[[:punct:]]" = "", " " = "-", "[^A-z-]"="")))
+  mutate(name = str_replace_all(`Chicken Breed Name`, c("[[:punct:]]" = "", " " = "-", "[^A-z-]"=""))) %>%
+  mutate(outfile = paste0("raw/chickens/", name, ".html"))
+
+
+purrr::walk2(breeds$link, breeds$outfile, ~read_html(.x) %>% xml2::write_html(.y))
+html <- read_html(url)
 
 download.file(breeds$link, destfile = paste0("raw/chickens/", breeds$name, ".html"))
